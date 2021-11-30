e81ccf3fb66529d3265ed55895ac42a1ff44b72e
lsx <1468709106@qq.com>
2021-11-30 21:54:00 +0800

    add db.sql

---
+++ db.sql
@@ -0,0 +1,20 @@
+CREATE DATABASE `db_apiserver`;
+
+USE `db_apiserver`;
+
+DROP TABLE IF EXISTS `tb_users`;
+
+CREATE TABLE `tb_users` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `createdAt` timestamp NULL DEFAULT NULL,
+  `updatedAt` timestamp NULL DEFAULT NULL,
+  `deletedAt` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`),
+  KEY `idx_tb_users_deletedAt` (`deletedAt`)
+);
+
+-- 默认添加了一条记录,用户名admin，密码admin
+INSERT INTO `tb_users` VALUES (0,'admin','$2a$10$veGcArz47VGj7l9xN7g2iuT9TF21jLI1YGXarGzvARNdnt4inC9PG','2018-05-27 16:25:33','2018-05-27 16:25:33',NULL);
